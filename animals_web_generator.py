50afdf6643338626423aa36919bc9efe7c2e088e
Work <Work@Mac-Air.fritz.box>
2025-01-30 15:55:14 +0100

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -9,28 +9,27 @@ animals_data = load_data('animals_data.json')
 
 output = ""
 for animal in animals_data:
+    output += '<li class="cards__item">'
     if 'name' in animal:
-        output += f"Name: {animal['name']}\n"
-
+        output += f"Name: {animal['name']}<br/>\n"
     if 'characteristics' in animal and 'diet' in animal['characteristics']:
-        output += f"Diet: {animal['characteristics']['diet']}\n"
-
+        output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
     if 'locations' in animal and animal['locations']:
-        output += f"Location: {animal['locations'][0]}\n"
-
+        output += f"Location: {animal['locations'][0]}<br/>\n"
     if 'characteristics' in animal and 'type' in animal['characteristics']:
-        output += f"Type: {animal['characteristics']['type']}\n\n"
+        output += f"Type: {animal['characteristics']['type']}<br/>\n"
+    output += '</li>'
 
 print(output)
 
 with open('animals_template.html', 'r') as file:
     content = file.read()
 
-print(content)
+#print(content)
 
 updated_content = content.replace('__REPLACE_ANIMALS_INFO__', output)
 
-print(updated_content)
+#print(updated_content)
 
 with open('animals.html', 'w') as file:
     file.write(updated_content)
