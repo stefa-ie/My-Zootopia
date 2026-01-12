1a4b4d2263340a7accb3c24e898529d1f336489e
Work <nguyen.stfn@gmail.com>
2026-01-12 15:08:54 +0100

    fix indentation in load_data

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,46 +1,54 @@
 import json
 
+
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+    """Loads a JSON file."""
+    with open(file_path, "r") as fobj:
+        return json.load(fobj)
 
-animals_data = load_data('animals_data.json')
 
-'''<li class="cards__item">
-  <div class="card__title">Wire Fox Terrier</div>
-  <p class="card__text">
-      <strong>Diet:</strong> Carnivore<br/>
-      <strong>Location:</strong> North-America and Canada<br/>
-      <strong>Type:</strong> mamal<br/>
-  </p>
-</li>'''
-
-output = ""
-for animal in animals_data:
-    output += '<li class="cards__item">\n'
-    if 'name' in animal:
-        output += f"<div class='card__title'>{animal['name']}</div><br/>\n"
+def serialize_animal(animal_obj):
+    """Serialize a single animal object to HTML."""
+    output = '<li class="cards__item">\n'
+    if 'name' in animal_obj:
+        output += f"<div class='card__title'>{animal_obj['name']}</div><br/>\n"
     output += "<p class='card__text'>\n"
-    if 'characteristics' in animal and 'diet' in animal['characteristics']:
-        output += f"<strong>Diet:</strong> {animal['characteristics']['diet']}<br/>\n"
-    if 'locations' in animal and animal['locations']:
-        output += f"<strong>Location:</strong> {animal['locations'][0]}<br/>\n"
-    if 'characteristics' in animal and 'type' in animal['characteristics']:
-        output += f"<strong>Type:</strong> {animal['characteristics']['type']}<br/>\n"
+    if 'characteristics' in animal_obj and 'diet' in animal_obj['characteristics']:
+        output += f"<strong>Diet:</strong> {animal_obj['characteristics']['diet']}<br/>\n"
+    if 'locations' in animal_obj and animal_obj['locations']:
+        output += f"<strong>Location:</strong> {animal_obj['locations'][0]}<br/>\n"
+    if 'characteristics' in animal_obj and 'type' in animal_obj['characteristics']:
+        output += f"<strong>Type:</strong> {animal_obj['characteristics']['type']}<br/>\n"
     output += '</p>\n'
     output += '</li>\n'
+    return output
 
-print(output)
 
-with open('animals_template.html', 'r') as file:
-    content = file.read()
+def serialize_all_animals(animals_data):
+    """Serialize all animals to HTML."""
+    output = ''
+    for animal_obj in animals_data:
+        output += serialize_animal(animal_obj)
+    return output
 
-#print(content)
 
-updated_content = content.replace('__REPLACE_ANIMALS_INFO__', output)
+def generate_html(template_path, animals_html, output_path):
+    """Generate HTML file from template and animals HTML."""
+    with open(template_path, 'r') as file:
+        content = file.read()
+    
+    updated_content = content.replace('__REPLACE_ANIMALS_INFO__', animals_html)
+    
+    with open(output_path, 'w') as file:
+        file.write(updated_content)
 
-#print(updated_content)
 
-with open('animals.html', 'w') as file:
-    file.write(updated_content)
+def main():
+    """Main function to orchestrate the HTML generation."""
+    animals_data = load_data('animals_data.json')
+    animals_html = serialize_all_animals(animals_data)
+    generate_html('animals_template.html', animals_html, 'animals.html')
+
+
+if __name__ == "__main__":
+    main()
